4d017f0ee1409cf794f79b7431aa9e59fd25f428
Guy Avraham <guy.avraham@velostrata.com>
2017-12-28 08:25:08 +0200

    BLOOM: added first interview question - WIP.

--- tasks/bloom/firstInterview.h
+++ tasks/bloom/firstInterview.h
@@ -1,17 +1,17 @@
 /*
-* First interview quesiotn:
+* First interview question:
 * -------------------------
 * Implement a simple regex match finder.
 * 1) Given alpha-bet for str is [a-z] only.
 * 2) (*) - means 0 chars or more.
 * 3) (?) - means 1 chars or more.
-* 4) If a givne pattern ends with a character --> than str MUST end with it as well. 
+* 4) If a given pattern ends with a character --> than str MUST end with it as well.
 * 5) A pattern can end with a "wild card" (? or *).
 * 6) You can assume pattern and str contains only [a-z] and [? *]  
 *
 * NOTES:
-* a) Initial code is the code providied below.
-* b) For the first implementaiton there is no need for efficeny -- just get it work.
+* a) Initial code is the code provided below.
+* b) For the first implementation there is no need for efficiency -- just get it work.
 */
 
 
@@ -28,7 +28,7 @@ bool isMatch(const char* str, const char* pattern)
 bool RunFirstInterviewQuestion()
 {
 	cout << "RunFirstInterviewQuestion - start" << endl;
-	cout << "isMatch(abcd, ab?d) returns:" << (isMatch("abcd", "ab?d") == true) << endl;	// sould return true
+	cout << "isMatch(abcd, ab?d) returns:" << (isMatch("abcd", "ab?d") == true) << endl;	// Should return true
 	cout << "isMatch(ab, a*b) returns:" << (isMatch("ab", "a*b") == true) << endl;		// should return true
 	cout << "isMatch(ab, a*b) returns:" << (isMatch("ab", "a?b") == true) << endl;		// should return false
 	cout << "RunFirstInterviewQuestion - end" << endl;
