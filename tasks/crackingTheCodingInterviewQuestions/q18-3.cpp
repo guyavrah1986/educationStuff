c4c8d6497478c51909c854a8877e28497f81b5be
Guy Avraham <guyavrah1986@gmail.com>
2018-01-13 17:28:26 +0200

    CRACKING_THE_CODE: implementation of question 18.3

---
+++ tasks/crackingTheCodingInterviewQuestions/q18-3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+class SingleTone
+{
+
+	public:
+		static SingleTone* GetInstance()
+		{
+			if (m_instance == 0)
+			{
+				m_mutex.lock();
+				if (m_instance == 0)
+				{
+					m_instance = new SingleTone();
+				}
+			}
+
+			return m_instance;
+		}
+	
+	private:
+		SingleTone()
+		{
+			cout << "SingleTone::SingleTone" << endl;
+		}
+
+
+
+	private:
+		static mutex m_mutex;
+		static SingleTone* m_instance;
+
+};
+
+SingleTone* SingleTone::m_instance = NULL;
+
+
+
+
+int main(int argc, char** argv)
+{
+	cout << "main - start" << endl;
+	SingleTone* firstInstance = GetInstance();
+
+
+	
+	
+	cout << "\n \n main - end" << endl;
+	return 0;
+}
