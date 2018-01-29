a21905a2e79cf84ee3e37662d605b56e582485c0
Guy Avraham <guy.avraham@velostrata.com>
2018-01-29 11:42:38 +0200

    CPP_STUFF: function template WIP.

--- cppStuff/functionTemplateExample.h
+++ cppStuff/functionTemplateExample.h
@@ -3,7 +3,9 @@
 *  Function templates:
 *  -------------------
 * 1) When we define a function template we define a "blue print" for a potential function instantiation. Nothing is being "created" (compiled) yet.
-*
+* 2) It is important to note that when we use template for user defined classes, than we should make sure that all functions and/or operators that are used within the template function
+*    are "well defined" (whether by us or by the compiler) as we wish it to be implemented.
+*    In case it is not defined we will recive a compilation error when the complier will generate the respective function that uses the user defined class.
 *
 *
 */
@@ -29,7 +31,6 @@ class A
 	private:
 	int m_a;
 };
-
 
 // 1)
 template <typename T> void mySwap(T& a, T& b)
