4bddb736b5bfd1d05d7c623e47ad97b62ecef24e
Guy Avraham <guyavrah1986@gmail.com>
2018-02-04 13:57:49 +0200

    SPIRENT_CODE: adapter mgr - WIP.

--- tasks/spirentCode/adapterMgr.cpp
+++ tasks/spirentCode/adapterMgr.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 
 #include "adapterMgr.h"
+#include "lwm2mAdapter.h"
 
 using namespace std;
 
 CAdapterMgr::CAdapterMgr(const list<EnProtocolType>& protocolsList)
 {
 	cout << "CAdapterMgr::CAdapterMgr - about to create CAdapterMgr for " << protocolsList.size() << " type of IoT protocols" << endl;
+	for (list<EnProtocolType>::const_iterator it = protocolsList.begin(); it != protocolsList.end(); ++it)
+	{
+		switch (*it)
+		{
+			case SP_M2M_PROTOCOL_TYPE_LWM2M: 
+							m_protocolAdapters.insert(pair < EnProtocolType, IAdapProtocol* > (*it, new CAdapterLwm2m()));
+							 cout << "CAdapterMgr::CAdapterMgr - adding LWM2M protocol adapter" << endl;
+							 break;
+
+			case SP_M2M_PROTOCOL_TYPE_MQTT: // TODO: !!m_protocolAdapters.insert(pair<EnProtocolType, IAdapProtocol*>(type, new CAdapterLwm2m()));
+							 cout << "CAdapterMgr::CAdapterMgr - adding MQTT protocol adapter" << endl;
+							 break;
+			default:  cout << "CAdapterMgr::CAdapterMgr - unsupported protocol adapter" << endl;
+				  break;
+		}
+	}
+
+	cout << "CAdapterMgr::CAdapterMgr - done initialize protocol adapters, with total of " << m_protocolAdapters.size() << " protocols added" << endl;
 }
 
 CAdapterMgr::~CAdapterMgr()
