dbd1e84177d63d42d0187da7dbc08920ba699678
Guy Avraham <guyavrah1986@gmail.com>
2018-02-05 22:41:08 +0200

    CYREN: add secind question initial draft.

---
+++ tasks/cyren/sortedArrayAndNumber.h
@@ -0,0 +1,18 @@
+// ===============================================================================================================================
+/*
+* Given a sorted array and a number - find two numbers that their sum eaquls to the given number.
+*/
+// ===============================================================================================================================
+
+
+#include <iostream>
+
+using namespace std;
+
+int arr [] = {1,3,5,6,9,10,45,67};
+
+void sortedArrAndNumberQuestion()
+{
+
+	
+}
