e2af836d6e5c38ef9c9cefa13ccd0750fd248d55
Guy Avraham <guyavrah1986@gmail.com>
2018-02-06 19:25:25 +0200

    VELOSTRATA_CODE: adding initial draft of SG file system.

---
+++ tasks/velostrataCode/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "sgFileSystem.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "main - start" << endl;
+
+
+
+	cout << "\n \n main - end" << endl;
+	return 0;
+}
