4af155baf0cb61de9999ee39a06f96887c80eafe
Guy Avraham <guyavrah1986@gmail.com>
2018-03-03 20:24:20 +0200

    MORE_EFFECTIVE: item 19 - added notes and example.

--- cppStuff/moreEffectiveCppVer1/item19.cpp
+++ cppStuff/moreEffectiveCppVer1/item19.cpp
@@ -1,6 +1,15 @@
 // ========================================================================================================================================================================
 /*
-*
+* Temporaries:
+* ------------
+* The "formal" definition of when temporary objects are created is when:A non heap object is created AND it is not named.
+* Practiaclly it happens in two main cases:
+* a) An implict conversion is made in a function call to make the passed argument being valid argument.
+* # By passing an object by value.
+* # When passing an object by non const reference - a good example will be the conversion of char arr[] into a string object (see example in the book).
+* 
+* b) When a function returns an object by value.
+* This is relevant almost in all cases, there are some exceptions though (the Return Value Optimization aka RVO is one of them).
 *
 */
 // ======================================================================================================================================================================
@@ -8,7 +17,6 @@
 #include <iostream>
 
 using namespace std;
-
 
 class A
 {
@@ -18,7 +26,7 @@ class A
 		cout << "A::A - setting m_a to:" << m_a << endl;
 	}
 
-	~A()
+	virtual ~A()
 	{
 		cout << "A::~A" << endl;
 	}
@@ -27,27 +35,40 @@ class A
 	{
 		cout << "A::A(copy)" << endl;
 	}
-	
-	A& operator=(const A& rhs)
-	{
-
-	}
 
 	int m_a;
-
 };
+
+
+void passArgByConstRef(const A& arg)
+{
+	cout << "passArgByConstRef - address of recived argument is:" << &arg << endl;
+}
+
+void passArgByValue(A arg)
+{
+	cout << "passArgByNonConstRef - address of recived argument is:" << &arg << endl;
+}
 
 void item19Usage()
 {
 	cout << "item19Usage - start" << endl;
 
-	cout << "\n \n item19Usage - " << endl;
+	A obj(17);
+	cout << "item19Usage - creating an object on the stack with address of:" << &obj << endl;
+	
+	passArgByConstRef(obj);	// no temp is created !!
+
+	passArgByValue(obj);	// a temp object is created and destroyed once the function returns !!
+
+	cout << "\n \n item19Usage - end" << endl;
 }
 
 int main(int argc, char** argv)
 {
 	cout << "main - start" << endl;
-
+	item19Usage();
+	
 	cout << "\n \n main - end" << endl;
 	return 0;
 }
