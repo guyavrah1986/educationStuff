a2684366317d85ba805794a0b6523219285996f2
guyavrah1986 <guyavrah1986@gmail.com>
2018-04-01 17:36:58 +0300

    MORE_EFFECTIVE_VER3: added first example with pointer to char.

--- cppStuff/moreEffectiveCppVer3/item3.cpp
+++ cppStuff/moreEffectiveCppVer3/item3.cpp
@@ -2,7 +2,10 @@
 /*
 * const pointer and pointer to const:
 * ----------------------------------- 
-* 
+* 1) When declaring a pointer we have two options: 
+* a) Non const ppinter to non const data.
+* ) Declaring the pointer as a const, meaning we can NOT modify the location we are pointing to --> the const keyword is on the LEFT side of the astrik (*).
+* ) Declraing the pointer to point to a const "object", meaning we can NOT modifiy the content that the pointer is pointing to.
 *
 */
 // ====================================================================================================================================================================
@@ -11,14 +14,27 @@
 
 using namespace std;
 
-void pointerUsageExample()
+void pointerToPrimitiveTypeUsageExample()
 {
+	cout << "pointerToPrimitiveTypeUsageExample - start" << endl;
+	// a) In both cases (p1 & p2) we can modify the content of the 
+	//    pointer and also the location it points to.
+	char p1 [] = "Hello"; 
+	char* p2 = p1;
+	*p2 = 'h';
 	
+	cout << "pointerToPrimitiveTypeUsageExample - p1 points to:" << p1 << endl;
+	cout << "pointerToPrimitiveTypeUsageExample - p2 points to:" << p2 << endl;
+
+	char* p3 = "Hello world"; 
+	//*p3 = 'h';
+	
+	cout << "\n \n pointerToPrimitiveTypeUsageExample - end" << endl;
 }
 
 void item3Usage()
 {
-	pointerUsageExample();
+	pointerToPrimitiveTypeUsageExample();
 	cout << "\n \n item3Usage - end" << endl;
 }
 
