ecd111ecca805e41ddf881b9ed4c1736966a7a87
guyavrah1986 <guyavrah1986@gmail.com>
2018-04-03 18:48:35 +0300

    MORE_EFFECTIVE_VER1: item 4 - WIP, added example with base class WITH default ctor.

--- cppStuff/moreEffectiveCppVer1/item4.cpp
+++ cppStuff/moreEffectiveCppVer1/item4.cpp
@@ -1,4 +1,4 @@
-// =============================================================================================================================================
+// =============================================================================================================================================================
 /*
 * item 4:
 * ------
@@ -8,11 +8,15 @@
 * a) In this case, we can see that it is impossible to declare (create) a "stack-Base1d" array of objects with no default Ctor.
 * b) The same goes when the araay is created on the heap.
 * 
-* 2) It is worth mentionng that most (maybe all) of the STL containers DOES NOT require to have a default ctor --> so in these cases
-*    there won't be any problem.
+* 2) Second, it is worth mentionng that most (maybe all) of the STL containers DOES NOT require to have a default ctor --> so in these cases
+*    there won't be any problem. Note, anyhow, that they do require that the objects that contained within them are copy-constructable
+*    and assginmentable.
 *
+* 3) Third, a case worth mention is with base classes. In case a base class, say, in a long "inheritence chain" DOES NOT have a default ctor,
+*    it implies that ALL derived classes will supply the base class values for its members (assuming there are). That is not such a "good constranit"
+*    to imply on the developers of a derived class.
 */
-// =============================================================================================================================================
+// ================================================================================================================================================================
 #include <iostream>
 #include <vector>
 #include <map>
@@ -59,6 +63,45 @@ class Base2
 	int m_b;
 };
 
+class Base3
+{
+	public:
+	Base3(int a) 
+		: m_a(a)
+	{
+		cout << "Base3::Base3 - setting m_a to:" << m_a << endl;
+	}
+
+	Base3()
+		: m_a(12)
+	{
+		cout << "Base3::Base3(default) - setting m_a to:" << m_a << endl;
+	}
+	
+	virtual ~Base3()
+	{
+		cout << "Base3::~Base3" << endl;
+	}
+
+	int m_a;
+};
+
+class Derived3 : public Base3
+{
+	public:
+	Derived3(int b)
+	{
+		cout << "Derived3::Derived3 - setting m_b to:" << m_b << endl;
+	}
+
+	virtual ~Derived3()
+	{
+		cout << "Derived3::Derived3" << endl;
+	}
+			
+	int m_b;
+};
+
 
 void noDefaultCtorIssueWithArraysInitializationExample()
 {
@@ -77,6 +120,15 @@ void noDefaultCtorIssueWithArraysInitializationExample()
 	//    assginmenable --> so this code will work.
 	map<Base1, int> m;
 	vector<Base1> vec1;
+	
+	cout << "noDefaultCtorIssueWithArraysInitializationExample - end" << endl;
+}
+
+void noDefaultCtorInBaseClassExample()
+{
+	cout << "noDefaultCtorInBaseClassExample - start" << endl;
+	Derived3(12);
+	cout << "\n \n noDefaultCtorInBaseClassExample - end" << endl;
 }
 
 void item4Usage()
@@ -84,6 +136,7 @@ void item4Usage()
 	cout << "item4Usage - start" << endl;
 
 	noDefaultCtorIssueWithArraysInitializationExample();
+	noDefaultCtorInBaseClassExample();
 	
 	cout << "\n \n item4Usage - end" << endl;
 }
