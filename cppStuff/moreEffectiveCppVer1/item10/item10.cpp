2ffba76528370bb3675244aaf56bc1ab0737bac8
Guy Avraham <guyavrah1986@gmail.com>
2018-04-10 16:29:42 +0300

    MORE_EFFECTIVE_VER1: item 10 - WIP

--- cppStuff/moreEffectiveCppVer1/item10/item10.cpp
+++ cppStuff/moreEffectiveCppVer1/item10/item10.cpp
@@ -8,10 +8,13 @@
 * So far we have understood the PROBLEM and it causes.
 *
 * 1) In this case - all good, no "worries".
-* 2) In this case, when intentioally we "simulate" an exception throw form the C class's ctor. It is important no note here, that, even
+* 2) In this case, when intentioally we "simulate" an exception throw form the C class's ctor. 
+* a) It is important no note here, that, even
 *    if we manually (explictly) call the dtor of class C AFTER the exception was thrown and caught - it won't help, cause a dtor of
 *    a class is relevant ONLY if the ctor of that class was run completly --> so we will have a memory leak (in the size of class A, cause
 *    it's ctor did run completly, YET its dtor did not...).
+* b) Any data member of class C (i.e.- not a pointer) is not effected from the fact that the dtor of class C is NOT invoked -- it is 
+*    being deleted ANYWAY !!
 *
 * Notes:
 * ) No need to check for NULL pointer before deleting the pointers of class C in the dtor (C++ permits to delete NULL pointers).
@@ -63,9 +66,10 @@ public:
 class C
 {
 public:
-	C(int a, int b)
+	C(int a, int b, const string& str)
 		: m_a(nullptr)
 		, m_b(nullptr)
+		, m_str(str)
 	{
 		m_a = new A(a);
 		cout << "C::C - setting m_a to a newly A object created on the heap (address):" << m_a << endl;
@@ -87,6 +91,7 @@ public:
 
 	A* m_a;
 	B* m_b;
+	string m_str; // 2b)
 };
 
 void item10Usage()
@@ -95,19 +100,19 @@ void item10Usage()
 
 	// 1) invoke a normal creation of a C object - on the stack
 	{
-		C c(1, 2);
+		C c(1, 2, "str1");
 	}
 
 	// 2)
 	C* pc = 0;
 	try
 	{
-		pc = new C(1, 0);
+		pc = new C(1, 0, "str2");
 	}
 	catch (const exception& e)
 	{
 		cout << "item10Usage - caught an exception while trying to create a new C object on the heap:" << e.what() << endl;
-		delete pc;
+		delete pc; // 2a)
 	}
 
 	cout << "\n \n item10Usage - " << endl;
