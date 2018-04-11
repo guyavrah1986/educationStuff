3f400c4be40ce7bda3163b7295f28687090fcf00
Guy Avraham <guyavrah1986@gmail.com>
2018-04-11 15:40:39 +0300

    MORE_EFFECTIVE_VER1: re-factoring the sample code.

--- cppStuff/moreEffectiveCppVer1/item12/item12.cpp
+++ cppStuff/moreEffectiveCppVer1/item12/item12.cpp
@@ -3,6 +3,11 @@
 * Item 12:
 * --------
 * Preface:
+* 1) Some key difference between passing an argument to a function than catching an exception in the catch clause:
+* a) When we throw an exception (which might be any "std home-made" OR user defined class/object) to a catch cluases, if the catch 
+*    clauses DOES NOT recive a pointer, meaning, it recives the exception "by reference" OR "by value" --> the object being thrown
+*    is copied (!!) from the throw site ("caller").
+* b) This is NOT the case with "simple" object passing to function's arguments.
 * 
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
@@ -56,9 +61,58 @@ public:
 	string m_str;
 };
 
+class B : public A
+{
+public:
+	B(const string& str, int b)
+		: A(str)
+		, m_b(b)
+	{
+		cout << "B::B - set m_b to:" << m_b << endl;
+	}
+
+	~B()
+	{
+		cout << "B::~B" << endl;
+	}
+
+	B(const B& other)
+		: B(other.m_str, other.m_b)
+	{
+		cout << "B::B(copy)" << endl;
+	}
+
+	int m_b;
+};
+
+void getAByRef(const A& a)
+{
+	cout << "getAByRef - got a object with address:" << &a << endl;
+}
+
+void diffBetweenCatchAndArgumentByValOrRef()
+{
+	cout << "diffBetweenCatchAndArgumentByValOrRef - start" << endl;
+	try
+	{
+		A a("A on function stack");
+		cout << "diffBetweenCatchAndArgumentByValOrRef - within try block, calling getAByRef, sending it a local object with address:" << &a << " as an argument" << endl;
+		getAByRef(a); // 1b)
+		cout << "diffBetweenCatchAndArgumentByValOrRef - within try block, throwing the same local A object as an exception" << endl;
+		throw a; // 1a)
+	}
+	catch (const A& a) // 1a) 
+	{
+		cout << "diffBetweenCatchAndArgumentByValOrRef - within catch block, caught an A object with address:" << &a << endl;
+	}
+
+}
+
 void item12Usage()
 {
 	cout << "item12Usage - start" << endl;
+
+	diffBetweenCatchAndArgumentByValOrRef();
 
 	cout << "\n \n item12Usage - end" << endl;
 }
