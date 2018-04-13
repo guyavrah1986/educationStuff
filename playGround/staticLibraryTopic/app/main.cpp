7a45ade6d8d46fe982de8a08077070b1de29aaa3
Guy Avraham <guyavrah1986@gmail.com>
2018-04-13 10:40:16 +0300

    STATIC_LIB_EXAMPLE: Added initial commit

---
+++ playGround/staticLibraryTopic/app/main.cpp
@@ -0,0 +1,24 @@
+// =============================================================================================================================================
+/*
+* main source file:
+* -----------------
+* This file holds the code for the application itself.
+*
+*/
+// =============================================================================================================================================
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "main - start" << endl;
+
+	char c = 0;
+	cout << "main - enter any key to terminate (and press enter)" << endl;
+	cin >> c;
+	cout << "\n \n main - end" << endl;
+	cout << "\n \n main - end" << endl;
+	return 0;
+}
--- playGround/staticLibraryTopic/lib1/classA.h
+++ playGround/staticLibraryTopic/lib1/classA.h
@@ -16,38 +16,14 @@
 *
 */
 // =============================================================================================================================================
-#include <iostream>
-#include <string>
 
-using namespace std;
+//#include "../lib2/classB.h"
 
-void item1Usage()
+class A
 {
-	cout << "item1Usage - start" << endl;
-	
-	// 1a)
-	//string& str; //--> won't compile
-	string str1("first string");
-	string str2("second string");
-	cout << "item1Usage - address of str1:" << &str1 << " its content:" << str1 << endl;
-	cout << "item1Usage - address of str2:" << &str2 << " its content:" << str2 << endl;
-	
-	string& ref1 = str1;
-	string* ptr1 = &str1;
+	public:
+	A(/*B& b*/);
+	~A();
 
-	cout << "item1Usge - ptr1:" << *ptr1 << " and it points to address" << ptr1 << endl;
-	ptr1 = &str2;
-	cout << "item1Usge - after re-assgin ptr1 = &str2,  *ptr1 is:" << *ptr1 << " and it points to address" << ptr1 << endl;
-	
-	// 1b) 
-	ref1 = str2;
-	cout << "item1Usge - after re-assgin ref1 = str2, str1 is:" << str1 << " it address is still:" << &str1 << endl;
-}
-
-int main(int argc, char** argv)
-{
-	cout << "main - start" << endl;
-	item1Usage();
-	cout << "\n \n main - end" << endl;
-	return 0;
-}
+	//B& m_b; // 1a)
+};
