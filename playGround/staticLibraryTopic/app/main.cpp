35782ace30966cafb339e8a6949b77aa92b0ebcd
Guy Avraham <guyavrah1986@gmail.com>
2018-04-13 14:24:46 +0300

    STATIC_LIB_EXAMPLE: removed the other "lib" (class B).

--- playGround/staticLibraryTopic/app/main.cpp
+++ playGround/staticLibraryTopic/app/main.cpp
@@ -2,18 +2,23 @@
 /*
 * main source file:
 * -----------------
-* This file holds the code for the application itself.
+* This file holds the code for the application itself. It is the "main" input that will produce our .exe file
+* This exe, among integral libraries, will be linked to
 *
 */
 // =============================================================================================================================================
 #include <iostream>
 #include <string>
 
+#include "../lib1/classA.h"
+
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int , char**)
 {
 	cout << "main - start" << endl;
+	A a;
+	a.FooA();
 
 	char c = 0;
 	cout << "main - enter any key to terminate (and press enter)" << endl;
