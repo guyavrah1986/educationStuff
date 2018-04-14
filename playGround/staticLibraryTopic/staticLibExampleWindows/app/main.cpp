710e8dd8594897d7f0ec08305f3804d7ade728f0
Guy Avraham <guyavrah1986@gmail.com>
2018-04-14 17:41:11 +0300

    STATIC_LIB_WIN_EXAMPLE: added initial commit

---
+++ playGround/staticLibraryTopic/staticLibExampleWindows/app/main.cpp
@@ -0,0 +1,24 @@
+// =============================================================================================================================================
+/*
+* main source file:
+* -----------------
+* This file holds the code for the application itself. It is the "main" input that will produce our .exe file
+* This exe, among integral libraries, will be linked to
+*
+*/
+// =============================================================================================================================================
+#include <iostream>
+
+using namespace std;
+
+int main(int , char**)
+{
+	cout << "main - start" << endl;
+
+	char c = 0;
+	cout << "main - enter any key to terminate (and press enter)" << endl;
+	cin >> c;
+	cout << "\n \n main - end" << endl;
+	cout << "\n \n main - end" << endl;
+	return 0;
+}
