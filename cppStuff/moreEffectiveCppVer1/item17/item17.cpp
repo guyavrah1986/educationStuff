b1f90ecab800f57d46650a7445675b1a5a58ee4d
Guy Avraham <guyavrah1986@gmail.com>
2018-04-20 19:03:09 +0300

    MORE_EFFECTIVE_VER1: added the initial draft for the sample class

--- cppStuff/moreEffectiveCppVer1/item17/item17.cpp
+++ cppStuff/moreEffectiveCppVer1/item17/item17.cpp
@@ -1,10 +1,67 @@
+// ========================================================================================================================================================================
+/*
+* Item 17:
+* --------
+* This item talk about the conecpt of "lazy evaluation" - which, bottom line means, initialize (or create) anything (usually class's
+* data members) as late as possible, i.e.- only when required. 
+* This concpets has several "impacts" on typical use cases, for instance:
+* 1) One way to
+*
+* !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
+* !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
+* Key note:
+* !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
+* !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
+*/
+// ======================================================================================================================================================================
 #include <iostream>
 
 using namespace std;
 
+class MyLazyEvaluationObj
+{
+public:
+	MyLazyEvaluationObj(int* pi, double* pd)
+		: m_pi(nullptr)
+		, m_pd(nullptr)
+	{
+		cout << "MyLazyEvaluationObj::MyLazyEvaluationObj" << endl;
+	}
+
+	~MyLazyEvaluationObj()
+	{
+		cout << "MyLazyEvaluationObj::~MyLazyEvaluationObj" << endl;
+	}
+
+	void doSomethingWithIntMember() const
+	{
+		cout << "MyLazyEvaluationObj::doSomethingWithIntMember" << endl;
+
+	}
+
+	void doSomethingWithDoubleMember() const
+	{
+		cout << "MyLazyEvaluationObj::doSomethingWithDoubleMember" << endl;
+	}
+
+	int* m_pi;
+	double* m_pd;
+};
+
+void item17Usage()
+{
+	cout << "item17Usage - start" << endl;
+
+
+
+	cout << "\n \n item17Usage - end" << endl;
+}
+
 int main(int argc, char** argv)
 {
 	cout << "main -start" << endl;
+
+	item17Usage();
 
 	cout << "\n \n main - end" << endl;
 	return 0;
