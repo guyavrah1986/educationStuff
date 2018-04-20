c9c69b3658b868956c7dc5817869548dfb23c2bd
Guy Avraham <guyavrah1986@gmail.com>
2018-04-20 19:30:47 +0300

    MORE_EFFECTIVE_VER1: item17 - WIP

--- cppStuff/moreEffectiveCppVer1/item17/item17.cpp
+++ cppStuff/moreEffectiveCppVer1/item17/item17.cpp
@@ -8,6 +8,8 @@
 * 1) The need to check whether a data member is NULL or not in each member function.
 * a) One way we can use lazy evalution is to initialize the data members to NULL pointers in the ctor 
 * b) Then, only upon demand to actually "fetch" (initialize) them.
+* c) In order to be able to perfomr the reuqired initialization in each member function (even those who are declared to be const) - we need 
+*    to use the mutable keyword, which allows us to set the pointer to some values EVEN WITHIN "const member functions".
 *
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
@@ -30,6 +32,13 @@ public:
 		cout << "MyLazyEvaluationObj::MyLazyEvaluationObj" << endl;
 	}
 
+	MyLazyEvaluationObj()
+		: m_pi(nullptr)
+		, m_pd(nullptr)
+	{
+		cout << "MyLazyEvaluationObj::MyLazyEvaluationObj(default)" << endl;
+	}
+
 	~MyLazyEvaluationObj()
 	{
 		cout << "MyLazyEvaluationObj::~MyLazyEvaluationObj" << endl; 
@@ -43,7 +52,7 @@ public:
 		if (m_pi == nullptr)
 		{
 			cout << "MyLazyEvaluationObj::doSomethingWithIntMember - m_pi is NULL, initializing it with some value" << endl;
-			//m_pi = new double;
+			m_pi = new int(17); // 1b)
 		}
 		else
 		{
@@ -57,7 +66,7 @@ public:
 		if (m_pd == nullptr)
 		{
 			cout << "MyLazyEvaluationObj::doSomethingWithDoubleMember - m_pd is NULL, initializing it with some value" << endl;
-			//m_pd = new double;
+			m_pd = new double(12.3); // 1b)
 		}
 		else
 		{
@@ -65,25 +74,32 @@ public:
 		}
 	}
 
-	int* m_pi;
-	double* m_pd;
+	mutable int* m_pi;		// 1c)
+	mutable double* m_pd;	// 1c)
 };
 
 void item17Usage()
 {
 	cout << "item17Usage - start" << endl;
-
-
-
+	cout << "item17Usage - creating a MyLazyEvaluationObj on the stack" << endl;
+	MyLazyEvaluationObj obj;
+	cout << "item17Usage - FIRST time calling its const member function" << endl;
+	obj.doSomethingWithIntMember();
+	cout << "item17Usage - SECOND time calling its const member function" << endl;
+	obj.doSomethingWithIntMember();
+	
 	cout << "\n \n item17Usage - end" << endl;
 }
 
 int main(int argc, char** argv)
 {
-	cout << "main -start" << endl;
+	cout << "main - start" << endl;
 
 	item17Usage();
 
+	char c = 0;
+	cout << "main - enter any key to terminate (and press enter)" << endl;
+	cin >> c;
 	cout << "\n \n main - end" << endl;
 	return 0;
 }
