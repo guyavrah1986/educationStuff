d6f1998d153900e6ee31d6be34f8e9aa42659b0b
Guy Avraham <guyavrah1986@gmail.com>
2018-04-20 19:18:41 +0300

    MORE_EFFECTIVE_VER1: item17 - WIP

--- cppStuff/moreEffectiveCppVer1/item17/item17.cpp
+++ cppStuff/moreEffectiveCppVer1/item17/item17.cpp
@@ -5,7 +5,9 @@
 * This item talk about the conecpt of "lazy evaluation" - which, bottom line means, initialize (or create) anything (usually class's
 * data members) as late as possible, i.e.- only when required. 
 * This concpets has several "impacts" on typical use cases, for instance:
-* 1) One way to
+* 1) The need to check whether a data member is NULL or not in each member function.
+* a) One way we can use lazy evalution is to initialize the data members to NULL pointers in the ctor 
+* b) Then, only upon demand to actually "fetch" (initialize) them.
 *
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
@@ -21,7 +23,7 @@ using namespace std;
 class MyLazyEvaluationObj
 {
 public:
-	MyLazyEvaluationObj(int* pi, double* pd)
+	MyLazyEvaluationObj(int* pi, double* pd) // 1a)
 		: m_pi(nullptr)
 		, m_pd(nullptr)
 	{
@@ -30,18 +32,37 @@ public:
 
 	~MyLazyEvaluationObj()
 	{
-		cout << "MyLazyEvaluationObj::~MyLazyEvaluationObj" << endl;
+		cout << "MyLazyEvaluationObj::~MyLazyEvaluationObj" << endl; 
+		delete m_pd;
+		delete m_pi;
 	}
 
 	void doSomethingWithIntMember() const
 	{
 		cout << "MyLazyEvaluationObj::doSomethingWithIntMember" << endl;
-
+		if (m_pi == nullptr)
+		{
+			cout << "MyLazyEvaluationObj::doSomethingWithIntMember - m_pi is NULL, initializing it with some value" << endl;
+			//m_pi = new double;
+		}
+		else
+		{
+			cout << "MyLazyEvaluationObj::doSomethingWithIntMember - m_pi is already initialized with value:" << *m_pi << endl;
+		}
 	}
 
 	void doSomethingWithDoubleMember() const
 	{
 		cout << "MyLazyEvaluationObj::doSomethingWithDoubleMember" << endl;
+		if (m_pd == nullptr)
+		{
+			cout << "MyLazyEvaluationObj::doSomethingWithDoubleMember - m_pd is NULL, initializing it with some value" << endl;
+			//m_pd = new double;
+		}
+		else
+		{
+			cout << "MyLazyEvaluationObj::doSomethingWithDoubleMember - m_pd is already initialized with value:" << *m_pd << endl;
+		}
 	}
 
 	int* m_pi;
