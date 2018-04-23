a9067b41ab3051bf6840de82051b10a347c8d363
Guy Avraham <guyavrah1986@gmail.com>
2018-04-23 22:21:59 +0300

    MORE_EFFEVTIVE_VER1: adding item 20 initial commit

--- cppStuff/moreEffectiveCppVer1/item20/item20.cpp
+++ cppStuff/moreEffectiveCppVer1/item20/item20.cpp
@@ -11,6 +11,55 @@
 
 using namespace std;
 
+class A
+{
+public:
+	A(int a)
+		: m_a(a)
+	{
+		cout << "A::A - setting m_a:" << m_a << " at this:" << this << endl;
+	}
+
+	~A()
+	{
+		cout << "A::~A - m_a:" << m_a << " at this:" << this << endl;
+	}
+
+	A(const A& other)
+		: A(other.m_a)
+	{
+		cout << "A::A(copy) - created A object with address:" << this << endl;
+	}
+
+	A& operator=(const A& rhs)
+	{
+		if (this == &rhs)
+		{
+			cout << "A::operator= - trying to self assgin rhs:" << &rhs << endl;
+			return *this;
+		}
+
+		this->m_a = rhs.m_a;
+		return *this;
+	}
+
+	A funcReturnByValueWithRVO(int a)
+	{
+		cout << "A::funcReturnByValueWithRVO" << endl;
+		return A(a);
+	}
+
+	A funcReturnByValueWithLocalObject(int a)
+	{
+		A stackA(a);
+		cout << "A::funcReturnByValueWithLocalObject - returning stack based A object with"
+			" address:" << &stackA << endl;
+		return stackA;
+	}
+
+	int m_a;
+};
+
 void item20Usage()
 {
 	cout << "item20Usage - start" << endl;
