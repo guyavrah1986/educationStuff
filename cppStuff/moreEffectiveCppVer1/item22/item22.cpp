ed4114f30aacdc6e7453361cf129af741e07585c
Guy Avraham <guyavrah1986@gmail.com>
2018-04-24 11:45:45 +0300

    MORE_EFFECTIVE_VER1: adding some notes for item 22

--- cppStuff/moreEffectiveCppVer1/item22/item22.cpp
+++ cppStuff/moreEffectiveCppVer1/item22/item22.cpp
@@ -2,8 +2,12 @@
 /*
 * Item 22:
 * --------
+* When implementing the operator@ (stand-alone) and operator@= (compund) we face different motivations to use each one 
+* of them.
+* 1) Note that we:
+* a) Enabling the RVO by NOT naming the return object (keeping it anonymous).
+* b) Implementing the stand-alone IN TERMS of the compund flavour.
 * 
-*
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * Key note: 
@@ -17,6 +21,43 @@
 
 using namespace std;
 
+class MyInt
+{
+public:
+	MyInt()
+		: m_int(0)
+	{
+		cout << "MyInt::MyInt - setting m_int to:" << m_int << " at address:" << this << endl;
+	}
+
+	MyInt(int val)
+		: m_int(val)
+	{
+		cout << "MyInt::MyInt - setting m_int to:" << m_int << " at address:" << this << endl;
+	}
+
+	~MyInt()
+	{
+		cout << "MyInt::~MyInt - m_int:" << m_int << " at address:" << this << endl;
+	}
+
+	MyInt& operator+=(const MyInt& rhs)
+	{
+		cout << "operator+=(const MyInt& rhs)" << endl;
+		this->m_int = this->m_int + rhs.m_int;
+		return *this;
+	}
+
+	friend const MyInt operator+(const MyInt& lhs, const MyInt& rhs);
+
+	int m_int;
+};
+
+const MyInt operator+(const MyInt& lhs, const MyInt& rhs)
+{
+	cout << "operator+(const MyInt& lhs, const MyInt& rhs)" << endl;
+	return MyInt(lhs.m_int) += rhs;	// 1)
+}
 
 void item22Usage()
 {
