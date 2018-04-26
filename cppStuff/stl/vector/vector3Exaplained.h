1b0c6894b7d00ea5977c91fa9af5b8053872c29c
Guy Avraham <guyavrah1986@gmail.com>
2018-04-26 11:00:26 +0300

    STD_VECTOR: added some more files for additional examples.

--- cppStuff/stl/vector/vector3Exaplained.h
+++ cppStuff/stl/vector/vector3Exaplained.h
@@ -1,6 +1,42 @@
 #pragma once
 
+#include "MyObj.h"
+#include "MyObjNoDefualtCtor.h"
 #include "MyObjsOperators.h"
+
+class ObjWithVectorAsClassMember
+{
+public:
+	ObjWithVectorAsClassMember()
+		: m_vec()
+	{
+		std::cout << "ObjWithVectorAsClassMember::ObjWithVectorAsClassMember" << std::endl;
+	}
+
+	~ObjWithVectorAsClassMember()
+	{
+		std::cout << "ObjWithVectorAsClassMember::~ObjWithVectorAsClassMember" << std::endl;
+	}
+
+	std::vector<MyObj> m_vec;
+};
+
+class ObjWithRefVectorAsClassMember
+{
+public:
+	ObjWithRefVectorAsClassMember()
+		: m_vec()
+	{
+		std::cout << "ObjWithRefVectorAsClassMember::ObjWithRefVectorAsClassMember" << std::endl;
+	}
+
+	~ObjWithRefVectorAsClassMember()
+	{
+		std::cout << "ObjWithRefVectorAsClassMember::~ObjWithRefVectorAsClassMember" << std::endl;
+	}
+
+	std::vector<MyObj> m_vec;
+};
 
 void objWithVectorClassMember();
 void objWithRefToVectorClassMember();
