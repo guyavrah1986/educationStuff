e1acde7ea0f291f4a6b1d7bbdea2fe76f96422ec
Guy Avraham <guyavrah1986@gmail.com>
2018-04-30 11:43:17 +0300

    MORE_EFFECTIVE_VER1: adding some more notes to note 2 item 27. Also added the third note.

--- cppStuff/moreEffectiveCppVer1/item27/item27.cpp
+++ cppStuff/moreEffectiveCppVer1/item27/item27.cpp
@@ -8,6 +8,15 @@
 * c) Also worth mention here is that we need to declare ONLY a single method (Dtor) as private (leaving the burdn
 *	 of verifying ALL the Ctor's are private).
 * 
+* 2) One issue that rises with the first approach is that these type of objects CAN NOT be base classes NOR can not
+*	 be contanined within other classes. In order to solve that, we can chose (for instance) the second approach
+*	 where the ctors are "private" - in this case protected, and the dtor is public.
+*	 In this approach we statisfy the need for the class to be created ONLY on the heap, with the capability to 
+*	 use this class as a base class OR to be contained witihn another class.
+* a) Offcourse we still need to provide some way to create the objects. 
+*  
+* 3) An additional NOT PORTABLE and not entirely complete appraoch to determine whether an address (of an object)
+*	 is on the heap or not is to use this function.
 *
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
 * !! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ !!
@@ -72,6 +81,65 @@ private:
 	const int* m_val;
 };
 
+class OnlyHeapObjProtectedCtors
+{
+protected:
+	OnlyHeapObjProtectedCtors(const int val)
+		: m_val(val)
+	{
+		cout << "OnlyHeapObjProtectedCtors::OnlyHeapObjProtectedCtors - setting m_val to:"
+			<< m_val << " at address:" << this << endl;
+	}
+
+	OnlyHeapObjProtectedCtors()
+		: m_val(0)
+	{
+		cout << "OnlyHeapObjProtectedCtors::OnlyHeapObjProtectedCtors(default) - setting m_val to:"
+			<< m_val << " at address:" << this << endl;
+	}
+
+	OnlyHeapObjProtectedCtors(const OnlyHeapObjProtectedCtors& other)
+		: m_val(other.m_val)
+	{
+		cout << "OnlyHeapObjProtectedCtors::OnlyHeapObjProtectedCtors(copy) - setting m_val to:"
+			<< m_val << " at address:" << this << endl;
+	}
+
+public:
+	static OnlyHeapObjProtectedCtors* createObj(int val = 0)	// 2a) 
+	{
+		cout << "OnlyHeapObjProtectedCtors::createObj" << endl;
+		return new OnlyHeapObjProtectedCtors(val);
+	}
+
+	~OnlyHeapObjProtectedCtors()
+	{
+		cout << "OnlyHeapObjProtectedCtors::~OnlyHeapObjProtectedCtors - m_val:" << m_val << " at"
+			" address:" << this << endl;
+	}
+
+	const int m_val;
+
+};
+
+bool onTheHeap(const void* address)
+{
+	char onTheLocalStack;
+	bool onHeap = false;
+	cout << "onTheHeap - got address:" << address << endl;
+	if (address < &onTheLocalStack)
+	{
+		cout << "onTheHeap -  address:" << address << " is on the HEAP" << endl;
+		onHeap = true;
+	}
+	else
+	{
+		cout << "onTheHeap -  address:" << address << " is on the STACK" << endl;
+	}
+
+	return onHeap;
+}
+
 void item27Usage()
 {
 	cout << "item27Usage - start" << endl;
@@ -79,7 +147,9 @@ void item27Usage()
 	OnlyHeapObjPrivateDtor* obj1 = new OnlyHeapObjPrivateDtor(12);	// 1) works !!
 	obj1->destroy();
 
-	//OnlyHeapObjPrivateDtor obj; 	// 1) Wont compile !!
+	//OnlyHeapObjPrivateDtor obj2; 	// 1) Wont compile !!
+
+	OnlyHeapObjProtectedCtors* obj3 = OnlyHeapObjProtectedCtors::createObj(9);	// 2)
 	cout << "\n \n item27Usage - end" << endl;
 }
 
