f57f8d60ce11d526c14831a9569015ec84a5e0b7
Guy Avraham <guyavrah1986@gmail.com>
2018-09-20 22:07:11 +0300

    INTERVIEW_QUESTIONS: Added notes.

--- interviewQuestions/forcePoint/main.cpp
+++ interviewQuestions/forcePoint/main.cpp
@@ -1,11 +1,33 @@
 // ==================================================================================================================
 // Question: 
 // ---------
+// 0) pre-interview:
 // Implement a "waiting" mechanis which is similar to barrier.
 // 
 // Assumptions in the solution:
 // ----------------------------
 // a. Writing & Reading the value of the global variables is atomic.
+//
+// 
+// 1) Two threads run function q1 untill completion. What will be the poissbile values of sum at the end ?
+// Answer:
+// - Max value (full synchronization) - 20. 
+// - Min value: 2 
+//
+// 2) Implement an algorithm to decide whether or not the string from the entire ASCII alpha bet (256 options) is 
+//    composed ONLY from characters within charSet.
+//    Best solution: Hold (insert) all the characters from charSet into a map, and than for each character in buffer
+//    to check whether this chracter is in the map or not.
+//    Run time: Lookup - O(Log(k)) X O(n) = O(log(k) * n).
+//
+// 3) How does a mutes is implemented ?
+//
+// 4) What is the diffenret in processes on 32/64 bit systems ?
+//
+// 5) IP and ethernet header fileds.
+// 
+// 
+// 
 // ==================================================================================================================
 
 #include <iostream>
@@ -16,7 +38,25 @@ using namespace std;
 size_t gFlag = 0;
 const size_t gTotalNumOfThreads = 2;
 
-void func()
+
+int sum = 0;
+
+void q1()
+{
+	for (size_t i = 0; i < 10; ++i)
+	{
+		sum++;
+	}
+}
+
+void q2()
+{
+
+	const char* buff = "zskjhksdksdjnfkjsnf"; 	// lenght N (N is very big)
+	const char* charSet = "ABCDEF";			// lenght K (K is small)	
+}
+
+void preInterviewQuestion()
 {
 	cout << "func - start, thread ID:" << this_thread::get_id() << endl;
 	gFlag++;
