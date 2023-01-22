0532ad54ee64e6d17e9a0acbe886f8c7e9cc8e4c
Guy Avraham <Guy.Avraham@ecitele.com>
2023-01-22 08:18:27 +0200

    Chapter9: adding some comments.

--- learnCpp/chapter8/autoType/main.cpp
+++ learnCpp/chapter8/autoType/main.cpp
@@ -5,7 +5,7 @@
 // 1) In C++ the auto keyword was introduced to enable the developer to "forget" about type declration. In this case the variable x will be of
 //    type int.
 // 2) In case one wishes to declare a variable using the auto keyword ("type") he needs to take into account that the const-ness of the variable 
-//    needs to be added excplicitly - so in this case d2 is NOT const doubel but "only" double.
+//    needs to be added excplicitly - so in this case d2 is NOT const double but "only" double.
 // 3) Note that the compiler needs, neverthelss, some sort of "clue" 
 // =================================================================================================================================================
 // =================================================================================================================================================
@@ -22,9 +22,6 @@ int main(int argc, char** argv)
 	const double d1 = 13.2;
 	auto d2 {d1}; // 2)
 	auto y; // 3)
-	
-
-
 	cout << "main - end" << endl;
 	return 0;
 }
