c6194eb143cc05bb080b0b686c8acaa559da7a28
Guy Avraham <Guy.Avraham@ecitele.com>
2023-02-16 08:36:55 +0200

    COND_VAR_EXAMPLE: added some more code

--- multiThreaded/conditionVariablesExampleC++11/usageExample.cpp
+++ multiThreaded/conditionVariablesExampleC++11/usageExample.cpp
@@ -11,46 +11,66 @@ condition_variable cv;
 string threadsData;
 bool ready = false;
 bool processed = false;
+int numOfIterations = 2;
  
 void worker_thread2()
 {
 	cout << "worker_thread2 - start" << endl;
-	// Wait until main() sends data
-	unique_lock lk(m);
-	cv.wait(lk, []{return ready;});
- 
-	// after the wait, we own the lock.
-	cout << "worker_thread2 is processing data\n";
-	threadsData += " after processing";
- 
-	// Send data back to main()
-	processed = true;
-	cout << "worker_thread2 signals data processing completed\n";
- 
-	// Manual unlocking is done before notifying, to avoid waking up
-	// the waiting thread only to block again (see notify_one for details)
-	lk.unlock();
-	cv.notify_one();
+	for (int i = 0; i < numOfIterations; ++i)
+	{
+		cout << "worker_thread2 - iteration:" << i << endl;
+		// Wait until main() sends data
+		cout << "worker_thread2 BEFORE unique_lock lk(m)" << endl;
+		unique_lock lk(m);
+		cout << "worker_thread2 AFTER unique_lock lk(m)" << endl;
+		cv.wait(lk, []{return ready;});
+	 	cout << "worker_thread2 AFTER cv.wait" << endl;
+		// after the wait, we own the lock.
+		cout << "bar" << endl;
+	 
+		// Send data back to main()
+		processed = true;
+
+	 
+		// Manual unlocking is done before notifying, to avoid waking up
+		// the waiting thread only to block again (see notify_one for details)
+		cout << "worker_thread2 BEFORE lk.unlock" << endl;
+		lk.unlock();
+		cout << "worker_thread2 AFTER lk.unlock" << endl;
+
+		cv.notify_one();
+		cout << "worker_thread2 AFTER cv.notify" << endl;
+		//ready = false;
+	}
 	cout << "worker_thread2 - end" << endl;
 }
 
 void worker_thread1()
 {
 	cout << "worker_thread1 - start" << endl;
-
-	// send data to the worker thread
+	for (int i = 0; i < numOfIterations; ++i)
 	{
-		lock_guard lk(m);
-		threadsData = "Example data";
-		ready = true;
-		cout << "worker_thread1 signals data ready for processing\n";
-	}
-	cv.notify_one();
- 	cout << "worker_thread1 AFTER calling cv.notify_one()" << endl;
-	// wait for the worker2 to finish
-	{
-		unique_lock lk(m);
-		cv.wait(lk, []{return processed;});
+		cout << "worker_thread1 - iteration:" << i << endl;
+		// send data to the worker thread
+		{
+			lock_guard lk(m);
+			cout << "worker_thread1 AFTER lock_guard lk(m)" << endl;
+			cout << "foo" << endl;
+			ready = true;
+			cout << "worker_thread1 signals data ready for processing\n";
+		}
+		cv.notify_one();
+		cout << "worker_thread1 AFTER  cv.notify_one()" << endl;
+		// wait for the worker2 to finish
+		{
+			cout << "worker_thread1 BEFORE unique_lock lk(m)" << endl;
+			unique_lock lk(m);
+			//lock_guard lk(m);
+	 		cout << "worker_thread1 AFTER calling unique_lock lk(m)" << endl;
+			cv.wait(lk, []{return processed;});
+	 		cout << "worker_thread1 AFTER cv.wait" << endl;
+			//ready = false;
+		}
 	}
 	cout << "worker_thread1 - end" << endl;
 }
