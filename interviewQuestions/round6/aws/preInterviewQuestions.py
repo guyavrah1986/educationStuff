62ad342ad17f9687bfd53f7f907e69f25e3b7c16
Guy Avraham <guyavrah1986@gmail.com>
2023-09-29 14:34:35 +0300

    Update preInterviewQuestions.py

--- interviewQuestions/round6/aws/preInterviewQuestions.py
+++ interviewQuestions/round6/aws/preInterviewQuestions.py
@@ -60,6 +60,115 @@ def find_max_range_of_power_of_two_usage_example():
     ret_val = find_max_range_of_power_of_two(arr4)
     print(func_name + "ret val is:" + str(ret_val))
 
+# use cases:
+# empty array
+# array with one interval
+# array with 3 time intervals:
+# 1. all distinct                               [[1, 3] , [5, 7], [8, 10]]
+# 2. the 2nd  is within the first entierly      [[1, 4] , [2, 3], [5, 7]]
+# 3. the 2nd is within the 1st and the 3rd      [[1, 4] , [2, 6], [5, 7]]
+# 4. the 2nd is within the 3rd entierly         [[1, 4] , [6, 7], [5, 8]]
+# 5. the 2nd is ends AFTER the 3rd              [[1, 4] , [2, 8], [5, 7]]
+
+# 6. the 1st ends AFTER the 3rd                 [[1, 10] , [2, 8], [5, 7]]
+
+def merge_intervals(intervals: list) -> list:
+    func_name = "merge_intervals - "
+    if intervals is None:
+        return None
+    
+    print(func_name + "got the original intervals array" + str(intervals))
+    intervals.sort()
+    print(func_name + "after sorting the original intervals array it is now:" + str(intervals))
+    
+    returned_list_of_merged_intervals = []
+    # the intervals array is sorted, this MUST be the start of the first interval
+    # (cause its start time is the minimal)
+    prev_start_interval = intervals[0][0]
+    
+    # for start assume it also for the end time of the first interval
+    prev_end_interval = intervals[0][1]
+    
+    for interval in intervals[1:]:
+        # 1. lower bound IN
+        if interval[0] < prev_end_interval:
+            
+            # 1.1 upper bound IN ([[1, 4] , [2, 3])
+            if interval[1] < prev_end_interval:
+                # The current iterated interval is enteirly within its predecessor, leave it out of the return list of intervals
+                continue
+        
+            # 1.2 upper bound OUT ([[1, 4] , [2, 6]) 
+            else:
+                # take the END interval as a new candidate
+                prev_end_interval = interval[1]
+                continue
+        
+        # 2. lower bound OUT 
+        else:
+            # take the entire prev_start and prev_end as an interval in the resulting list
+            returned_list_of_merged_intervals.append([prev_start_interval, prev_end_interval])
+            
+            # set the current interval as the "new prev"
+            prev_start_interval = interval[0]
+            prev_end_interval = interval[1]
+            continue
+    
+    returned_list_of_merged_intervals.append([prev_start_interval, prev_end_interval])
+    return returned_list_of_merged_intervals
+   
+def merge_intervals_usage_example():
+    func_name = "merge_intervals_usage_example - "
+    intervals1 = [[1, 4] , [2, 3]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals1))
+    ret_list = merge_intervals(intervals1)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals2 = [[1, 4] , [5, 6]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals2))
+    ret_list = merge_intervals(intervals2)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals3 = [[1, 4] , [2, 6]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals3))
+    ret_list = merge_intervals(intervals3)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals4 = [[2, 3]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals4))
+    ret_list = merge_intervals(intervals4)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals5 = [[1, 3] , [5, 7], [8, 10]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals5))
+    ret_list = merge_intervals(intervals5)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals = [[1, 4] , [2, 3], [5, 7]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals))
+    ret_list = merge_intervals(intervals)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals = [[1, 4] , [2, 6], [5, 7]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals))
+    ret_list = merge_intervals(intervals)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals = [[1, 4] , [6, 7], [5, 8]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals))
+    ret_list = merge_intervals(intervals)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals = [[1, 4] , [2, 8], [5, 7]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals))
+    ret_list = merge_intervals(intervals)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
+    intervals = [[1, 10] , [2, 8], [5, 7]]
+    print(func_name + "about to call the merge interval function for:" + str(intervals))
+    ret_list = merge_intervals(intervals)
+    print(func_name + "the returned intervals merged list is:" + str(ret_list))
+    
 if __name__ == "__main__":
     print("main - start")
     
